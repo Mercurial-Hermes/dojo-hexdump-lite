151d586de2b8e19a58674eda383101cd22674105
elusivenode <elusivenode@gmail.com>
2025-12-27 15:51:15 +1000

    act 3 hdl add user width choice

--- src/main.zig
+++ src/main.zig
@@ -5,16 +5,34 @@ pub fn main() !void {
     var stdout = std.io.getStdOut().writer();
     var stderr = std.io.getStdErr().writer();
 
+    var width: usize = 16;
     _ = args.next(); // consume arg[0]
 
-    const trg_file_path = args.next() orelse {
+    const arg1 = args.next() orelse {
         try printUsage();
         return error.InvalidUsage;
     };
 
+    var trg_file_path: []const u8 = undefined;
+
+    if (std.mem.eql(u8, arg1, "--width")) {
+        const width_str = args.next() orelse {
+            try stderr.print("Missing value for --width\n", .{});
+            return error.InvalidUsage;
+        };
+        width = try std.fmt.parseInt(usize, width_str, 10);
+        trg_file_path = args.next() orelse {
+            try printUsage();
+            return error.InvalidUsage;
+        };
+    } else {
+        trg_file_path = arg1;
+    }
+
     const file = try std.fs.cwd().openFile(trg_file_path, .{ .mode = .read_only });
     defer file.close();
 
+    var col: usize = 0;
     var offset: usize = 0;
     const CHUNK_SIZE = 16;
     var reader = file.reader();
@@ -29,12 +47,25 @@ pub fn main() !void {
         if (n == 0) break;
 
         for (buf[0..n]) |b| {
-            try stdout.print("{d:8} {x:0>2}\n", .{ offset, b });
+            if (col == 0) {
+                try stdout.print("{d:8} ", .{offset});
+            }
+
+            try stdout.print("{x:0>2} ", .{b});
+
             offset += 1;
+            col += 1;
+
+            if (col == width) {
+                try stdout.print("\n", .{});
+                col = 0;
+            }
         }
     }
 
-    try stdout.print("\n", .{});
+    if (col != 0) {
+        try stdout.print("\n", .{});
+    }
 }
 
 fn printUsage() !void {
